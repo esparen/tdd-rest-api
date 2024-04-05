24b81e58987cd254a8d6e71708a9bec0b65fbc39
Eder Sparenberger <edsparen@gmail.com>
2024-04-05 11:26:11 -0300

    feat(users): adds include new user

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 const express = require('express');
 
-const app = express();
 const port = 3001;
+
+const app = express();
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+
+var users = [
+  {name:'John Doe', email: 'email@email.com' }
+]
 
 app.get('/', (req, res) => {
   res.status(200).send()
 });
 
-app.get('/users', (req, res) => {
-  const users = [
-    {name:'John Doe', email: 'email@email.com' }
-  ]
+app.get('/users', (req, res) => {  
   res.status(200).json(users);
+});
+
+app.post('/users', (req, res) => {
+  console.log('req.body', req.body);
+  users.push(req.body);
+  res.status(201).json(req.body);
 });
 
 module.exports = app ;
--- test/user.test.js
+++ test/user.test.js
@@ -4,12 +4,21 @@ const app = require("../src/app");
 
 describe('users', () => {
   
-  it('Should list all the users', async () => {
+  it('GET on the route "/users" should list all the users', async () => {
     const response = await supertest(app).get('/users');
     expect(response.status).toBe(200);
     expect(response.body).toHaveLength(1);
     expect(response.body[0]).toHaveProperty('name', 'John Doe');
 
   });
+
+  it('POST on the route "/users" should insert a new record ', async () => {
+    const newUser = {name: 'Walter Mitty', email: 'watermitty@email.com'}
+    const response = await supertest(app).post('/users').send(newUser);
+    expect(response.status).toBe(201);
+    expect(response.body.name).toBe(newUser.name);
+    expect(response.body.email).toBe(newUser.email) ;
+
+  })
 
 });
